649fe70808f009618a81a803da940c4c2b9de93c
FordUniver <61389961+FordUniver@users.noreply.github.com>
2024-02-10 22:25:01 +0100

    fixed some versions

--- lakefile.lean
+++ lakefile.lean
@@ -2,15 +2,11 @@ import Lake
 open Lake DSL
 
 package «TheBook» where
-  -- add package configuration options here
+  -- add any package configuration options here
 
-lean_lib «TheBook» where
-  -- add library configuration options here
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"@"v4.5.0"
 
 @[default_target]
-lean_exe «thebook» where
-  root := `Main
-  -- Enables the use of the Lean interpreter by the executable (e.g.,
-  -- `runFrontend`) at the expense of increased binary size on Linux.
-  -- Remove this line if you do not need such functionality.
-  supportInterpreter := true
+lean_lib «TheBook» where
+  -- add any library configuration options here
