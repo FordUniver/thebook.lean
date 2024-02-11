7455ed209aa99f96e5f3d5e01ad0a2d47dfc2af4
FordUniver <61389961+FordUniver@users.noreply.github.com>
2024-02-11 14:21:16 +0100

    hopefully bug fix

--- TheBook.lean
+++ TheBook.lean
@@ -1,5 +1,5 @@
 -- This module serves as the root of the `TheBook` library.
 -- Import modules here that should be built as part of the library.
 import TheBook.NumberTheory.InfinitudePrimes.EuclidProof
-import TheBook.NumberTheory.InfinitudePrimes.Goldbachproof
+import TheBook.NumberTheory.InfinitudePrimes.GoldbachProof
 import TheBook.NumberTheory.InfinitudePrimes.MathlibProof
