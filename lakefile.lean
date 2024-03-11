836534e3ed39b61c5b3489dfc5d9fa333d26507b
FordUniver <61389961+FordUniver@users.noreply.github.com>
2024-03-11 11:49:38 +0100

    updated to v4.7.0-rc2

--- lakefile.lean
+++ lakefile.lean
@@ -5,7 +5,7 @@ package «TheBook» where
   -- add any package configuration options here
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"@"v4.5.0"
+  "https://github.com/leanprover-community/mathlib4.git"@"v4.7.0-rc2"
 
 @[default_target]
 lean_lib «TheBook» where
