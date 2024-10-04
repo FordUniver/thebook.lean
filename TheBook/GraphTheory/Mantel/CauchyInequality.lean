da54e46b9b739ec840237b3d1816e361b345f572
Christoph Spiegel <christophspiegel.berlin@gmail.comm>
2024-10-04 08:40:45 +0200

    minor changes before lean bump

--- TheBook/GraphTheory/Mantel/CauchyInequality.lean
+++ TheBook/GraphTheory/Mantel/CauchyInequality.lean
@@ -13,9 +13,6 @@ variable {V : Type*} [Fintype V] [DecidableEq V]
 variable (G : SimpleGraph V) [DecidableRel G.Adj]
 instance : Fintype G.edgeSet := G.fintypeEdgeSet
 
-#check Finset.sum_mul_sq_le_sq_mul_sq -- probably the Cauchy-Schwarz inequality we want
-#check Finset.sum_card_bipartiteAbove_eq_sum_card_bipartiteBelow -- probably the double counting equality we want
-
 lemma degree_eq_number_of_incident_edges (v : V) : G.degree v = Finset.card (Finset.filter (λ e => v ∈ e) G.edgeFinset) := by sorry
 
 lemma edges_have_two_incident_vertices (e : Sym2 V) {h: e ∈ G.edgeFinset} : Finset.card (Finset.filter (λ v => v ∈ e) (@Finset.univ V _)) = 2 := by
@@ -28,14 +25,20 @@ lemma handshake : ∑ v : V, G.degree v = 2 * G.edgeFinset.card := by
   let V' := @Finset.univ V _
 
   calc ∑ v : V, G.degree v
+  --  _ = ∑ v ∈ V', Finset.card {e ∈ E | v ∈ e} := by sorry --simp [degree_eq_number_of_incident_edges]
    _ = ∑ v ∈ V', Finset.card (Finset.filter (λ e => v ∈ e) E) := by simp [degree_eq_number_of_incident_edges]
    _ = ∑ e ∈ E, Finset.card (Finset.filter (λ v => v ∈ e) V') := Finset.sum_card_bipartiteAbove_eq_sum_card_bipartiteBelow (λ v e => v ∈ e)
+  --  _ = ∑ e ∈ E, Finset.card {v ∈ V' | v ∈ e} := Finset.sum_card_bipartiteAbove_eq_sum_card_bipartiteBelow (λ v e => v ∈ e)
    _ = ∑ e ∈ E, 2 := by sorry -- edges_have_two_incident_vertices
    _ = 2 * E.card := by simp [V', edges_have_two_incident_vertices, Nat.mul_comm]
+
+
+#check Finset.sum_mul_sq_le_sq_mul_sq -- probably the Cauchy-Schwarz inequality we want
 
 theorem mantel (h: G.CliqueFree 3) : G.edgeFinset.card ≤ (Fintype.card V)^2 / 4 := by
   let n := Fintype.card V
   let E := G.edgeFinset
+  let V' := @Finset.univ V _
 
   have (i j : V) (h: G.Adj i j) : G.degree i + G.degree j ≤ n := by sorry
   let sum_degrees_of_edge (e : Sym2 V) : ℕ := Sym2.lift ⟨λ x y => G.degree x + G.degree y, λ x y => by simp [Nat.add_comm]⟩ e
@@ -43,9 +46,13 @@ theorem mantel (h: G.CliqueFree 3) : G.edgeFinset.card ≤ (Fintype.card V)^2 / 
   have : ∑ (e ∈ E), sum_degrees_of_edge e = ∑ (v : V), (G.degree v)^2 := by sorry
   have : E.card*n ≥ ∑ (v : V), (G.degree v)^2 := by sorry
   have : ∑ (v : V), (G.degree v)^2 ≥ (∑ (v : V), G.degree v)^2 / n := by sorry
-  have : (∑ (v : V), G.degree v)^2 / n = 4 * E.card^2 / n := by sorry -- SimpleGraph.sum_degrees_eq_twice_card_edges
+  have : (∑ (v : V), G.degree v)^2 / n = (2 * E.card)^2 / n := by rw [handshake]
+  have : (2 * E.card)^2 = 4 * E.card^2 := by linarith
+  have : (2 * E.card)^2 / n = 4 * E.card^2 / n := by aesop
+  
   sorry
 
+-- Probably needs to use floor and ceil to be precise ...
 theorem mantel_equality (h: G.CliqueFree 3) : 0 = 0 := by
   sorry
 
