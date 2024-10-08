e74560c5094d28800fc16e083c65904b13206c83
Christoph Spiegel <christophspiegel.berlin@gmail.comm>
2024-10-08 15:26:52 +0200

    moved cauchy schwarz into mantel

--- TheBook/GraphTheory/Mantel/CauchyInequality.lean
+++ TheBook/GraphTheory/Mantel/CauchyInequality.lean
@@ -8,7 +8,7 @@ import Mathlib.Combinatorics.SimpleGraph.Clique
 import Mathlib.Combinatorics.SimpleGraph.DegreeSum
 import Mathlib.Combinatorics.Enumerative.DoubleCounting
 import Mathlib.Algebra.Order.BigOperators.Ring.Finset
-import Mathlib.Algebra.Order.Chebyshev
+import Mathlib.Algebra.Order.BigOperators.Ring.Finset
 -- import Mathlib.Tactic.Basic
 import Aesop
 
@@ -33,14 +33,14 @@ local notation "n" => Fintype.card α
 
 attribute [simp] Nat.pow_two
 attribute [simp] Finset.sum_congr
+attribute [simp] Finset.one_lt_card
+attribute [simp] Finset.card_ne_zero_of_mem
 attribute [aesop safe] Finset.mul_sum
 attribute [aesop safe] Finset.card_eq_sum_ones
 attribute [aesop safe] SimpleGraph.adj_symm
 attribute [aesop safe] SimpleGraph.Adj.ne
-attribute [aesop safe] sq_sum_le_card_mul_sum_sq
 attribute [aesop safe] HMul.hMul
-attribute [aesop safe] MonovaryOn.sum_mul_sum_le_card_mul_sum
-attribute [aesop safe] AntivaryOn.card_mul_sum_le_sum_mul_sum
+attribute [aesop safe] Sym2.Mem.other
 
 -- edges have two vertices
 lemma card_edge_eq_two (e : Sym2 α) (he : e ∈ E): #{v | v ∈ e} = 2 := by
@@ -49,25 +49,26 @@ lemma card_edge_eq_two (e : Sym2 α) (he : e ∈ E): #{v | v ∈ e} = 2 := by
     use v, w, (by aesop)
     ext; simp
 
+-- degree of v is the sum of the indicator functions of the edges containing v
 lemma aux (e : Sym2 α) (v : α) : #{w' ∈ V | s(v,w') = e} = χ(v ∈ e) := by
   by_cases hve : v ∈ e
-  · simp [hve]
-    by_contra hc
-    let S := {w' ∈ V | s(v,w') = e}
+  · let S := {w' ∈ V | s(v,w') = e}
     have : Sym2.Mem.other hve ∈ S := by aesop
-    have := Finset.one_lt_card.mp ((Nat.two_le_iff (#S)).mpr ⟨Finset.card_ne_zero_of_mem this, hc⟩)
+    have : #S ≠ 0 := Finset.card_ne_zero_of_mem this
+    by_contra hc; simp [hve] at hc
+    have : ∃ a ∈ S, ∃ b ∈ S, a ≠ b := Finset.one_lt_card.mp ((Nat.two_le_iff (#S)).mpr ⟨this, hc⟩)
     aesop
   · aesop
 
 lemma deg_sum_edges (v : α) : d(v) = ∑ e ∈ E, χ(v ∈ e) := by
   calc d(v)
-    _ = #{w | G.Adj v w}.toFinset              := rfl
-    _ = ∑ w ∈ V, χ(G.Adj v w)                  := by simp
-    _ = ∑ w ∈ V, ∑ e ∈ E, χ(s(v,w) = e)        := by simp
-    _ = ∑ e ∈ E, ∑ w ∈ V, χ(s(v,w) = e)        := Finset.sum_comm
-    _ = ∑ e ∈ E, ∑ w ∈ {w' ∈ V | s(v,w') = e}, 1   := by simp
-    _ = ∑ e ∈ E, #{w' ∈ V | s(v,w') = e}           := by simp
-    _ = ∑ e ∈ E, χ(v ∈ e)                      := by simp [aux]
+    _ = #{w | G.Adj v w}.toFinset                := rfl
+    _ = ∑ w ∈ V, χ(G.Adj v w)                    := by simp
+    _ = ∑ w ∈ V, ∑ e ∈ E, χ(s(v,w) = e)          := by simp
+    _ = ∑ e ∈ E, ∑ w ∈ V, χ(s(v,w) = e)          := Finset.sum_comm
+    _ = ∑ e ∈ E, ∑ w ∈ {w' ∈ V | s(v,w') = e}, 1 := by simp
+    _ = ∑ e ∈ E, #{w' ∈ V | s(v,w') = e}         := by simp
+    _ = ∑ e ∈ E, χ(v ∈ e)                        := by simp [aux]
 
 lemma handshake : ∑ v ∈ V, d(v) = 2 * #E := by
   calc  ∑ v ∈ V, d(v)
@@ -78,7 +79,13 @@ lemma handshake : ∑ v ∈ V, d(v) = 2 * #E := by
     _ = 2 * ∑ e ∈ E, 1                := Eq.symm (Finset.mul_sum E (λ _ ↦ 1) 2)
     _ = 2 * #E                        := by simp
 
-lemma cauchyschwarz (f : α → ℕ): n * ∑ v ∈ V, (f v)^2 ≥ (∑ v ∈ V, f v)^2 := by sorry
+-- lemma cauchyschwarz (f : α → ℕ): n * ∑ v ∈ V, (f v)^2 ≥ (∑ v ∈ V, f v)^2 := by
+--   let one (_ : α) : ℕ := 1
+--   calc  n * (∑ v ∈ V, (f v)^2)
+--     _ = (∑ v ∈ V, (f v)^2) * n                  := by simp [Nat.mul_comm]
+--     _ = (∑ v ∈ V, f v^2) * (∑ v ∈ V, (one v)^2) := by simp [one]
+--     _ ≥ (∑ v ∈ V, f v * one v)^2                := Finset.sum_mul_sq_le_sq_mul_sq V f one
+--     _ = (∑ v ∈ V, f v)^2                        := by simp [one]
 
 theorem mantel (h: G.CliqueFree 3) : #E ≤ (n^2 / 4) := by
 
@@ -98,26 +105,20 @@ theorem mantel (h: G.CliqueFree 3) : #E ≤ (n^2 / 4) := by
     obtain ⟨hik, hjk⟩ := h
 
     -- -- But then i, j, k would form a triangle, contradicting the assumption that G has no 3-cliques
-    have has_card : #{k, j, i} = 3 := by
-      apply SimpleGraph.Adj.ne' at hij
-      apply SimpleGraph.Adj.ne' at hik
-      apply SimpleGraph.Adj.ne' at hjk
-      simp [hij, hik, hjk]
-
-    exact h {k, j, i} ⟨by aesop, has_card⟩ 
+    exact h {k, j, i} ⟨by aesop, by simp [hij.ne', hik.ne', hjk.ne']⟩ 
 
   let sum_deg (e : Sym2 α) : ℕ := Sym2.lift ⟨λ x y ↦ d(x) + d(y), by simp [Nat.add_comm]⟩ e
 
   have sum_deg_ub (e : Sym2 α) (he: e ∈ E) : sum_deg e ≤ n := by obtain ⟨i, j⟩ := e; aesop
 
-  have aux (e : Sym2 α) (he: e ∈ E) : sum_deg e = ∑ (v ∈ V), d(v) * χ(v ∈ e) := by sorry
+  have aux (e : Sym2 α) (he: e ∈ E) : sum_deg e = ∑ v ∈ V, d(v) * χ(v ∈ e) := by sorry
   
-  have sum_sum_deg_eq_sum_sum_sq : ∑ (e ∈ E), sum_deg e = ∑ (v ∈ V), d(v)^2 := by
+  have sum_sum_deg_eq_sum_sum_sq : ∑ (e ∈ E), sum_deg e = ∑ v ∈ V, d(v)^2 := by
     calc  ∑ (e ∈ E), sum_deg e
-      _ =  ∑ (e ∈ E), ∑ (v ∈ V), d(v) * χ(v ∈ e)     := Finset.sum_congr rfl aux
-      _ =  ∑ (v ∈ V), (∑ (e ∈ E), (d(v) * χ(v ∈ e))) := Finset.sum_comm
-      _ =  ∑ (v ∈ V), (d(v) * (∑ (e ∈ E), χ(v ∈ e))) := Finset.sum_congr rfl (λ v a ↦ Eq.symm (Finset.mul_sum E (λ i ↦ χ(v ∈ i)) d(v)))
-      _ =  ∑ (v ∈ V), d(v)^2                         := Finset.sum_congr rfl sorry
+      _ =  ∑ (e ∈ E), ∑ v ∈ V, d(v) * χ(v ∈ e)     := Finset.sum_congr rfl aux
+      _ =  ∑ v ∈ V, (∑ (e ∈ E), (d(v) * χ(v ∈ e))) := Finset.sum_comm
+      _ =  ∑ v ∈ V, (d(v) * (∑ (e ∈ E), χ(v ∈ e))) := Finset.sum_congr rfl (λ v a ↦ Eq.symm (Finset.mul_sum E (λ i ↦ χ(v ∈ i)) d(v)))
+      _ =  ∑ v ∈ V, d(v)^2                         := Finset.sum_congr rfl sorry
 
   -- have t1 : n^2 * ∑ (e ∈ E), 1 = n * ∑ (e ∈ E), n := by
   --   have : n * ∑ (e ∈ E), 1 = ∑ (e ∈ E), n * 1 := Finset.mul_sum E (λ i ↦ 1) n
@@ -125,17 +126,20 @@ theorem mantel (h: G.CliqueFree 3) : #E ≤ (n^2 / 4) := by
   --   linarith
 
   -- We slightly modify the argument to avoid division (in particular by zero)
+  let one (_ : α) : ℕ := 1
   have := calc n^2 * #E
-    _ = n^2 * ∑ (e ∈ E), 1          := by simp
-    _ = (n * n) * ∑ (e ∈ E), 1      := by simp
-    _ = n * (n * ∑ (e ∈ E), 1)      := by linarith
-    _ = n * (∑ (e ∈ E), n * 1)      := by sorry  -- FIX
-    _ = n * (∑ (e ∈ E), n)          := by simp
-    _ ≥ n * ∑ (e ∈ E), sum_deg e    := Nat.mul_le_mul_left n (Finset.sum_le_sum sum_deg_ub) -- FIX
-    _ = n * ∑ (v ∈ V), d(v)^2       := by simp [sum_sum_deg_eq_sum_sum_sq]
-    _ ≥ (∑ (v ∈ V),  d(v))^2        := cauchyschwarz (λ v ↦ d(v))
-    _ = (2 * #E)^2                  := by simp [handshake]
-    _ = 4 * #E^2                    := by linarith
+    _ = n^2 * ∑ (e ∈ E), 1                       := by simp
+    _ = (n * n) * ∑ (e ∈ E), 1                   := by simp
+    _ = n * (n * ∑ (e ∈ E), 1)                   := by linarith
+    _ = n * (∑ (e ∈ E), n * 1)                   := by sorry  -- FIX
+    _ = n * (∑ (e ∈ E), n)                       := by simp
+    _ ≥ n * ∑ (e ∈ E), sum_deg e                 := Nat.mul_le_mul_left n (Finset.sum_le_sum sum_deg_ub)
+    _ = n * ∑ v ∈ V, d(v)^2                      := by simp [sum_sum_deg_eq_sum_sum_sq]
+    _ = (∑ v ∈ V, one v^2) * ∑ v ∈ V, d(v)^2     := by simp [one]
+    _ ≥ (∑ v ∈ V, one v * d(v))^2                := Finset.sum_mul_sq_le_sq_mul_sq V one (λ v ↦ d(v))
+    _ = (∑ v ∈ V, d(v))^2                        := by simp [one]
+    _ = (2 * #E)^2                               := by simp [handshake]
+    _ = 4 * #E^2                                 := by linarith
 
   -- now show #E ≤ n^2 / 4 by "simply" dividing by 4 * #E
   by_cases hE : #E = 0
@@ -150,5 +154,7 @@ theorem mantel (h: G.CliqueFree 3) : #E ≤ (n^2 / 4) := by
 theorem mantel_equality (h: G.CliqueFree 3) : 0 = 0 := by
   sorry
 
+
+example ( a b : ℕ) (h : a ≤ b) : b ≥ a := by exact h
 
 end CauchyMantelTheorem
