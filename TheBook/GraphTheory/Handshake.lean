685e9f5557fc0d1f35b0fa40bff9aa044491e381
Christoph Spiegel <christophspiegel.berlin@gmail.comm>
2024-10-09 16:15:25 +0200

    double counting instead if indicator variable

--- TheBook/GraphTheory/Handshake.lean
+++ TheBook/GraphTheory/Handshake.lean
@@ -1,5 +1,6 @@
 import Mathlib.Combinatorics.SimpleGraph.Finite
 import Mathlib.Algebra.Order.BigOperators.Ring.Finset
+import Mathlib.Combinatorics.Enumerative.DoubleCounting
 
 variable {α : Type*} [Fintype α] [DecidableEq α]
 variable {G : SimpleGraph α} [DecidableRel G.Adj]
@@ -8,18 +9,17 @@ prefix:100 "#" => Finset.card
 local notation "E" => G.edgeFinset
 local notation "d(" v ")" => G.degree v
 local notation "I(" v ")" => G.incidenceFinset v
-local notation "χ(" p ")" => if p then 1 else 0
 
 lemma edge_card_eq_two (e : Sym2 α) (he : e ∈ E): #{v | v ∈ e} = 2 := by
   rw [Finset.card_eq_two]
   induction e with | _ v w => use v, w, (by aesop); ext; simp
 
-lemma handshake : ∑ v, d(v) = 2 * #E := by
+-- Proof through direct calculation
+lemma handshaking : ∑ v, d(v) = 2 * #E := by
   calc  ∑ v, d(v)
     _ = ∑ v, #I(v)                := by simp [G.card_incidenceFinset_eq_degree]
-    _ = ∑ v, ∑ e ∈ E, χ(v ∈ e)    := by simp [G.incidenceFinset_eq_filter]
-    _ = ∑ e ∈ E, ∑ v, χ(v ∈ e)    := Finset.sum_comm
-    _ = ∑ e ∈ E, #{v | v ∈ e}     := by simp
+    _ = ∑ v, #{e ∈ E | v ∈ e}     := by simp [G.incidenceFinset_eq_filter]
+    _ = ∑ e ∈ E, #{v | v ∈ e}     := Finset.sum_card_bipartiteAbove_eq_sum_card_bipartiteBelow _
     _ = ∑ e ∈ E, 2                := Finset.sum_congr rfl (λ e he ↦ edge_card_eq_two e he)
     _ = 2 * ∑ e ∈ E, 1            := (Finset.mul_sum E (λ _ ↦ 1) 2).symm
     _ = 2 * #E                    := by rw [Finset.card_eq_sum_ones E]
