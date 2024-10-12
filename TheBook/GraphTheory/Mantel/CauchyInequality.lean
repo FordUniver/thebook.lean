f76a74256d2a97333b8c8d71ec33d336f83891e2
Christoph Spiegel <christophspiegel.berlin@gmail.comm>
2024-10-12 09:50:08 +0200

    Further cleanup

--- TheBook/GraphTheory/Mantel/CauchyInequality.lean
+++ TheBook/GraphTheory/Mantel/CauchyInequality.lean
@@ -45,15 +45,15 @@ theorem mantel (h: G.CliqueFree 3) : #E ≤ (n^2 / 4) := by
   -- We need to define the sum of the degrees of the vertices of an edge ...
   let sum_deg (e : Sym2 α) : ℕ := Sym2.lift ⟨λ x y ↦ d(x) + d(y), by simp [Nat.add_comm]⟩ e
 
-  -- ... and establish a variant of adj_degree_bnd ...
+  -- ... and establish a (1) variant of adj_degree_bnd ...
   have adj_degree_bnd' (e : Sym2 α) (he: e ∈ E) : sum_deg e ≤ n := by
     induction e with | _ v w => simp at he; exact adj_degree_bnd v w (by simp [he])
 
-  -- ... as well as ...
+  -- ... as well as (2) a simple identity for sum_deg ...
   have sum_deg_eq (e : Sym2 α) (he: e ∈ E) : sum_deg e = ∑ v ∈ e, d(v) := by
     induction e with | _ v w => simp at he; simp [sum_deg, he.ne]
 
-  -- ... and finally
+  -- ... and finally (3) the identity for the sum of the squares of the degrees ...
   have sum_sum_deg_eq_sum_deg_sq : ∑ e ∈ E, sum_deg e = ∑ v ∈ V, d(v)^2 := by
     calc  ∑ e ∈ E, sum_deg e
       _ = ∑ e ∈ E, ∑ v ∈ e, d(v)                  := Finset.sum_congr rfl sum_deg_eq
@@ -62,7 +62,7 @@ theorem mantel (h: G.CliqueFree 3) : #E ≤ (n^2 / 4) := by
       _ = ∑ v ∈ V, ∑ e ∈ I(v), d(v)               := Finset.sum_congr rfl (λ v ↦ by simp [G.incidenceFinset_eq_filter v])
       _ = ∑ v ∈ V, d(v)^2                         := by simp [Nat.pow_two]
 
-  -- We slightly modify the main argument to avoid division by a potentially zero n ...
+  -- We now slightly modify the main argument to avoid division by a potentially zero n ...
   have := calc #E * n^2
     _ = (n * (∑ e ∈ E, 1)) * n               := by simp [Nat.pow_two, Nat.mul_assoc, Nat.mul_comm]
     _ = (∑ e ∈ E, n) * n                     := by rw [Finset.mul_sum]; simp
@@ -72,7 +72,7 @@ theorem mantel (h: G.CliqueFree 3) : #E ≤ (n^2 / 4) := by
     _ = (2 * #E)^2                           := by simp [G.sum_degrees_eq_twice_card_edges]
     _ = 4 * #E^2                             := by ring
 
-  -- ... and now show #E ≤ n^2 / 4 by "simply" dividing by 4 * #E
+  -- ... and show #E ≤ n^2 / 4 by "simply" dividing by 4 * #E
   by_cases hE : #E = 0
   · aesop
   · push_neg at hE
