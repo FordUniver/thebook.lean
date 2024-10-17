1056d569651684a102ad692a4747e8f234cfc1ab
ovi <ovi@posteo.de>
2024-10-17 15:16:45 +0200

    incorporating pr comments

--- TheBook/GraphTheory/Mantel/ArithmeticGeometricMean.lean
+++ TheBook/GraphTheory/Mantel/ArithmeticGeometricMean.lean
@@ -4,55 +4,59 @@ import Mathlib.Analysis.MeanInequalities -- has am-gm
 import TheBook.ToMathlib.WeightedDoubleCounting
 
 
+namespace SimpleGraph
+
+variable {α : Type*} (G : SimpleGraph α)
+
+-- An independent set in a graph is a set of vertices that are pairwise nonadjacent.
+abbrev IsIndependentSet (s : Set α) : Prop :=
+  s.Pairwise (fun a b => ¬ G.Adj a b)
+
+end SimpleGraph
+
+
+namespace AMGMMantelTheorem
+
 variable {α : Type*} (G : SimpleGraph α)
 variable [Fintype α] [DecidableEq α] [DecidableRel G.Adj] {n : ℕ} {a b c : α} {s : Finset α}
 
-
--- An independent set in a graph is a set of vertices that are pairwise nonadjacent.
-abbrev IsIndependentSet (G : SimpleGraph α) (s : Set α) : Prop :=
-  s.Pairwise (fun a b => ¬ G.Adj a b)
-
-
--- All independent sets of a graph.
-def allIndependentSets : Finset (Finset α) := {s | IsIndependentSet G s}
-
--- TODO these could go in a common file, no?
-local notation "V" => @Finset.univ α _
+-- TODO move to seperate file
+local notation "V" => @Finset.univ _ _
 local notation "E" => G.edgeFinset
 local notation "N(" v ")" => G.neighborFinset v
 local notation "d(" v ")" => G.degree v
 local notation "n" => Fintype.card α
 
+-- TODO move to ToMathlib
 prefix:100 "#" => Finset.card
---set_option trace.Meta.Tactic.simp true
 
 
 -- The neighbors of a vertex i form an independent set in a trianlge free graph G.
-lemma neighbors_independent (h: G.CliqueFree 3) : IsIndependentSet G N(i) := by
+lemma neighbors_independent (h: G.CliqueFree 3) : G.IsIndependentSet N(i) := by
   by_contra nind
-  rw [IsIndependentSet, Set.Pairwise] at nind
+  rw [SimpleGraph.IsIndependentSet, Set.Pairwise] at nind
   rw [SimpleGraph.CliqueFree] at h
-  aesop -- TODO why yello! is aesop even ok?
-  have : (i ≠ w) := by aesop
-  have : (i ≠ w_1) := by aesop
-  exact h {i, w, w_1} ⟨ by aesop , by aesop ⟩
+  simp [SimpleGraph.IsIndependentSet, Set.Pairwise] at nind
+  obtain ⟨j, _, k, _, _, _⟩ := nind
+  have : (i ≠ j) := by aesop
+  have : (i ≠ k) := by aesop
+  exact h {i, j, k} ⟨ by aesop , by aesop ⟩
 
 
 -- TODO can i say A is a maximal independent set in a less verbose way plz
 -- The degree of a vertex i is less or equal α, the size of a largest independent set.
 lemma degreeLeqa (h: G.CliqueFree 3) (A : Finset α) :
-    (∀ B ∈ (allIndependentSets G), #B ≤ #A) → d(i) ≤ #A := by
-  have : N(i) ∈ allIndependentSets G := by rw [allIndependentSets]; -- TODO why ∈ so difficult!
-                                            aesop;
-                                            apply neighbors_independent;
-                                            apply h
+    (∀ I : Finset α , G.IsIndependentSet I → #I ≤ #A) → d(i) ≤ #A := by
+  have : G.IsIndependentSet N(i) := by rw [SimpleGraph.IsIndependentSet]
+                                       apply neighbors_independent
+                                       apply h
   intro maxA
   apply maxA N(i) this
 
 
 -- The set B = V \ A meets every edge of G.
-lemma B_meets_every_edge {A : Finset α} (indA : IsIndependentSet G A) :
-    (∀ I ∈ (allIndependentSets G), #I ≤ #A) → (∀ e ∈ E, 1 ≤ #{ b ∈ (V \ A) | b ∈ e }) := by
+lemma B_meets_every_edge {A : Finset α} (indA : G.IsIndependentSet A) :
+    (∀ I : Finset α, G.IsIndependentSet I → #I ≤ #A) → (∀ e ∈ E, 1 ≤ #{ b ∈ (V \ A) | b ∈ e }) := by
 
     intro maxA e edgee
 
@@ -65,13 +69,13 @@ lemma B_meets_every_edge {A : Finset α} (indA : IsIndependentSet G A) :
        apply (b_nin_e _ this)
        simp
      . have : w ∉ (V \ A) := by by_contra wninA; apply (b_nin_e _ wninA); simp
-       rw [IsIndependentSet] at indA
+       rw [SimpleGraph.IsIndependentSet] at indA
        aesop
 
 
 -- We count the edges of G by counting the endvertices in B.
-lemma count_edges_by_B {A : Finset α} (indA : IsIndependentSet G A) :
-    (∀ I ∈ (allIndependentSets G), #I ≤ #A) → #E ≤ ∑ i ∈ V \ A, d(i) := by
+lemma count_edges_by_B {A : Finset α} (indA : G.IsIndependentSet A) :
+    (∀ I : Finset α, G.IsIndependentSet I → #I ≤ #A) → #E ≤ ∑ i ∈ V \ A, d(i) := by
 
    -- The number of edges adjacent to i is the degree of i. -- TODO duh?
    have n_adj_edges_eq_deg : ∀ i, #{e ∈ E | i ∈ e} = d(i) := by
@@ -95,28 +99,20 @@ lemma count_edges_by_B {A : Finset α} (indA : IsIndependentSet G A) :
 -- The inequality of the arithmetic and geometric mean.
 lemma am_gm (a b : ℕ) : 4 * a * b ≤ (a + b)^2 := by
   have := two_mul_le_add_sq a b -- mathlib version of the am-gm.
-  linarith -- is this ok to use?
-/- we could also...
-  calc 4 * a * b
-   _ = (2 * 2) * a * b := by simp only [Nat.reduceMul]
-   _ = 2 * (2 * a * b) := by repeat rw [mul_assoc]
-   _ = 2 * a * b + 2 * a * b := two_mul _
-   _ ≤ (a^2 + b^2) + 2 * a * b := by simp [this]
-   _ = (a + b)^2 := by simp [add_sq']
--/
+  linarith
 
 
 -- Mantel's Theorem
-theorem mantel (h: G.CliqueFree 3) (A : Finset α) (indA : IsIndependentSet G A) :
-    (∀ B ∈ (allIndependentSets G), #B ≤ #A) → #E ≤ (n^2 / 4) := by
+theorem mantel (h: G.CliqueFree 3) (A : Finset α) (indA : G.IsIndependentSet A) :
+    (∀ I : Finset α, G.IsIndependentSet I → #I ≤ #A) → #E ≤ (n^2 / 4) := by
 
   intro maxA
 
   have := calc #E
-   _ ≤ ∑ i ∈ V \ A, d(i)   := count_edges_by_B G indA maxA
-   _ ≤ ∑ _ ∈ V \ A, #A     := Finset.sum_le_sum (fun a _ => degreeLeqa G h A maxA)
-   _ = #(V \ A) * #A       := Finset.sum_const _ -- TODO simp can do this, do we prefer simp?
-   _ = (#V - #A) * #A      := by simp [Finset.card_sdiff _]
+   _ ≤ ∑ i ∈ V \ A, d(i)      := count_edges_by_B G indA maxA
+   _ ≤ ∑ _ ∈ V \ A, #A        := Finset.sum_le_sum (fun a _ => degreeLeqa G h A maxA)
+   _ = #(V \ A) * #A          := Finset.sum_const _
+   _ = (#V - #A) * #A         := by simp [Finset.card_sdiff _]; aesop
 
   have := calc #E * 4 -- TODO how annoying
    _ = 4 * #E                 := mul_comm _ _
@@ -128,3 +124,5 @@ theorem mantel (h: G.CliqueFree 3) (A : Finset α) (indA : IsIndependentSet G A)
    _ = n^2                    := by simp
 
   exact ((Nat.le_div_iff_mul_le' (by simp)).mpr this)
+
+end AMGMMantelTheorem
