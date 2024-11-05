1edd96b168c1ac8794d2445c1faf31345005a398
Christoph Spiegel <christophspiegel.berlin@gmail.comm>
2024-11-05 09:51:20 +0100

    readded imports

--- TheBook.lean
+++ TheBook.lean
@@ -4,8 +4,8 @@ import TheBook.ToMathlib.CliqueNumber
 
 import TheBook.NumberTheory.InfinitudePrimes.Statement
 import TheBook.NumberTheory.InfinitudePrimes.EuclidProof
--- import TheBook.NumberTheory.InfinitudePrimes.GoldbachProof
--- import TheBook.NumberTheory.InfinitudePrimes.FolkloreMersenneProof
--- import TheBook.NumberTheory.InfinitudePrimes.FurstenbergProof
+import TheBook.NumberTheory.InfinitudePrimes.GoldbachProof
+import TheBook.NumberTheory.InfinitudePrimes.FolkloreMersenneProof
+import TheBook.NumberTheory.InfinitudePrimes.FurstenbergProof
 
 import TheBook.GraphTheory.Mantel.CauchyInequality
