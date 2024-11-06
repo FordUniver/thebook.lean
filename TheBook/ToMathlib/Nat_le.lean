5c3f55a709a97a7ae3c08a96196af966977c2dbb
ovi <ovi@posteo.de>
2024-11-06 16:37:59 +0100

    added pr link, fix thm statement

--- TheBook/ToMathlib/Nat_le.lean
+++ TheBook/ToMathlib/Nat_le.lean
@@ -1,5 +1,7 @@
 import Mathlib.Data.Nat.Defs
 
+-- https://github.com/leanprover-community/mathlib4/pull/18694
+
 namespace Nat
 
 theorem le_div_iff_mul_le2' {a b c : Nat} (hb : 0 < b) : a ≤ c / b ↔ b * a ≤ c := by
