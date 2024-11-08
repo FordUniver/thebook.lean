4319f48d45a2461f6cd0e83ff7d75a0df59a9f6f
Christoph Spiegel <christophspiegel.berlin@gmail.comm>
2024-11-08 06:42:49 +0100

    minor  changes

--- TheBook/GraphTheory/Mantel/ArithmeticGeometricMean.lean
+++ TheBook/GraphTheory/Mantel/ArithmeticGeometricMean.lean
@@ -28,18 +28,18 @@ theorem mantel (h: G.CliqueFree 3) : #E ≤ n^2 / 4 := by
   --  Let `α` be the size of a largest independent set `A`, ...
   let α := SimpleGraph.cocliqueNum G
   let ⟨A, maxA⟩  := G.maximumIndependentSet_exists
-  have hA : #A = α := maximumIndependentSet_card_eq_cocliqueNum G A maxA
+  have hA : #A = α := maximumIndependentSet_card_eq_cocliqueNum _ _ maxA
 
   -- ... and set `β = n - α`.
   let β := n - α
-  have hαβ: α + β = n := Nat.add_sub_of_le (le_of_eq_of_le (hA.symm) (card_le_card (subset_univ A)))
+  have hαβ: α + β = n := Nat.add_sub_of_le (le_of_eq_of_le (hA.symm) (card_le_card (subset_univ _)))
 
   -- The neighbor set of a vertex `i` is an independent set.
   have nbhd_ind_of_triangle_free : ∀ (i : γ), G.IsIndependentSet N(i) := by
     simp [Set.coe_toFinset, G.isIndependentSet_neighborSet_if_triangleFree h, neighborFinset]
 
   -- The degree of a vertex `i` is less or equal the cardinality of a maximum independent set.
-  have degree_leq_cardA : ∀ (i : γ) , d(i) ≤ α := fun i => 
+  have degree_le_alpha : ∀ (i : γ) , d(i) ≤ α := fun i => 
     hA ▸ (le_of_le_of_eq (nbhd_ind_of_triangle_free i).card_le_cocliqueNum
                   (G.maximumIndependentSet_card_eq_cocliqueNum A maxA).symm)
 
@@ -47,14 +47,14 @@ theorem mantel (h: G.CliqueFree 3) : #E ≤ n^2 / 4 := by
   let B := V \ A
   have hB : #B = β := by have := card_sdiff (subset_univ A); simp_all [hαβ]
 
-  have one_geq_n_adj_verts : ∀ e ∈ E, 1 ≤ #{ i ∈ B | i ∈ e } := by
+  have one_ge_num_incident_verts : ∀ e ∈ E, 1 ≤ #{ i ∈ B | i ∈ e } := by
     simp only [one_le_card, mem_edgeFinset]
     exact G.compl_independentSet_meets_every_edge maxA.independentSet
 
   -- We count the edges of `G` by counting the endvertices in `B`.
   have count_edges_by_B := by calc
     #E = ∑ e ∈ E, 1                          := by simp
-     _ ≤ ∑ e ∈ E, #{ i ∈ B | i ∈ e }         := sum_le_sum one_geq_n_adj_verts
+     _ ≤ ∑ e ∈ E, #{ i ∈ B | i ∈ e }         := sum_le_sum one_ge_num_incident_verts
      _ = ∑ e ∈ E, ∑ i ∈ {i ∈ B | i ∈ e}, 1   := by simp
      _ = ∑ i ∈ B, ∑ e ∈ {e ∈ E | i ∈ e}, 1   := sum_sum_bipartiteAbove_eq_sum_sum_bipartiteBelow _ _
      _ = ∑ i ∈ B, #{e ∈ E | i ∈ e}           := by simp
@@ -65,12 +65,12 @@ theorem mantel (h: G.CliqueFree 3) : #E ≤ n^2 / 4 := by
   -- The final chain of inequalities.
   have card_E_bound := calc
     #E ≤ ∑ i ∈ B, d(i)      := by exact count_edges_by_B
-     _ ≤ ∑ _ ∈ B, α         := sum_le_sum fun _ _ => degree_leq_cardA _
+     _ ≤ ∑ _ ∈ B, α         := sum_le_sum fun _ _ => degree_le_alpha _
      _ = α * β              := by simp [Nat.mul_comm, hB]
 
   have four_times_card_E_bd := calc 
     4 * #E ≤ 4 * α * β        := by linarith
-         _ ≤ (α + β)^2        := four_mul_le_pow_two_add α β
+         _ ≤ (α + β)^2        := four_mul_le_pow_two_add _ _
          _ = n^2              := by simp only [hαβ, Nat.sub_add_cancel]
 
   exact (Nat.le_div_iff_mul_le_comm Nat.ofNat_pos).mpr four_times_card_E_bd
